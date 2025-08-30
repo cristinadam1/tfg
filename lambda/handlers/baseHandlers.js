d18ba52e2345bb6a479e4fda3f6728070fad2dc5
cristinadam1 <cristinadam@correo.ugr.es>
2025-08-30 12:53:51 +0200

    Actalización de guía

--- lambda/handlers/baseHandlers.js
+++ lambda/handlers/baseHandlers.js
@@ -56,17 +56,17 @@ const GameExplanationHandler = {
         
         if (intentName === 'AMAZON.YesIntent') {
             const explanation = generateSpeech(
-                '¡Perfecto! Encantada de conoceros, mi nombre es Alexa' +
+                '¡Perfecto! Encantada de conoceros, mi nombre es Alexa.' +
                 'Os explico cómo jugar a "Regreso al Pasado". ' +
-                'Es un juego de preguntas para que recordemos tiempos pasados y nos divirtamos un rato todos juntos. ' +
+                'Es un juego de preguntas, para que recordemos tiempos pasados, y nos divirtamos un rato todos juntos. ' +
                 'Primero, me diréis cuántos sois y vuestros nombres. ' +
                 'Luego, cada uno me dirá su canción favorita para conoceros un poco mejor ' +
                 'Después comenzarán las preguntas: algunas individuales y otras en equipo. ' +
                 'Para responder, debéis decir "creo que es" seguido de vuestra respuesta. ' +
                 'Si no sabéis una respuesta, podéis pedir ayuda diciendo "ayuda" o "necesito ayuda". ' +
                 'Al final, veremos quién ha recordado más momentos del pasado. ' +
-                'Y muy importante, no olvideis que si me quedo dormida debeis llamarme por mi nombre que es Alexa para que os escuche' +
-                '¿Qué os parece? ¡Vamos a empezar! ¿Cuántos jugadores sois hoy?'
+                'Y muy importante, no olvideis que si me quedo dormida, debeis llamarme por mi nombr, que es Alexa, para que os escuche' +
+                '¿Qué os parece.? ¡Vamos a empezar.! ¿Cuántos jugadores sois hoy?'
             );
             
             return handlerInput.responseBuilder
@@ -74,7 +74,7 @@ const GameExplanationHandler = {
                 .reprompt('¿Cuántos jugadores sois hoy? Por ejemplo: "somos 3 jugadores"')
                 .getResponse();
         } else if (intentName === 'AMAZON.NoIntent') {
-            const speakOutput = generateSpeech('¡De acuerdo! Vamos directos al juego. ¿Cuántos jugadores sois hoy?', true);
+            const speakOutput = generateSpeech('¡De acuerdo! Vamos directos a la partida. ¿Cuántos jugadores sois hoy?', false);
             
             return handlerInput.responseBuilder
                 .speak(speakOutput)

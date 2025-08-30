846018ded38aa065a9c7ad1c5b5b34bb86b536f3
cristinadam1 <cristinadam@correo.ugr.es>
2025-08-30 19:18:08 +0200

    Preguntas actualizadas

--- lambda/game/questions.js
+++ lambda/game/questions.js
@@ -1,49 +1,26 @@
 module.exports = {
-  IMAGEN: [
+  VIAJES: [
     {
       question: "¿Qué ciudad se conoce como 'La ciudad del amor'?",
-      answers: ["París.", "Paris.", "ciudad de París.", "ciudad de Paris."],
+      answers: ["París", "ciudad de París.", "la ciudad de Paris"],
       hints: ["Está en Francia.", "Tiene una torre muy famosa."],
       difficulty: 1,
       photo: "francia.png"
     },
     {
-      question: "¿Cuál es el plato típico de Italia que se hace con masa y tomate?",
-      answer: "pizza.",
-      hints: ["Se hace al horno.", "Suele tener queso."],
-      difficulty: 1,
-      photo: "pizza.png"
-    },
-    {
-      question: "¿Qué animal se conoce como el 'rey de la selva'?",
-      answer: "león",
-      hints: ["Tiene melena", "Es un felino grande"],
-      difficulty: 1,
-      photo: "leon.png"
-    },
-    {
       question: "¿Qué monumento de Egipto tiene cabeza humana y cuerpo de león?",
-      answers: ["esfinge", "efinge"],
+      answers: ["esfinge", "efinge", "la esfinge", "el esfinge"],
       hints: ["Está en Giza", "Es muy antigua"],
       difficulty: 2,
       photo: "esfinge.png"
     },
     {
-      question: "¿Cómo se llama el animal que vive en el Polo Sur?",
-      answer: "oso polar",
-      hints: ["Es blanco","Come focas", "Es muy grande y tiene pelo"],
+      question: "¿Cuál es el plato típico de Italia que se hace con masa y tomate?",
+      answers: ["pizza", "la pizza", "pisa", "la pisa", "pixa", "la pixa"],
+      hints: ["Se hace al horno.", "Suele tener queso."],
       difficulty: 1,
-      photo: "oso_polar.png"
+      photo: "pizza.png"
     },
-    {
-      question: "¿Qué fruta tropical es amarilla por fuera y blanca por dentro?",
-      answer: "plátano",
-      hints: ["Se pela", "Es rica en potasio", "A los monos les gusta mucho"],
-      difficulty: 1,
-      photo: "platano.png"
-    }
-  ],
-  VIAJES: [
     {
       question: "¿En qué país se pueden visitar las pirámides de Giza?",
       answer: "Egipto",
@@ -125,21 +102,21 @@ module.exports = {
   PAISAJES: [
     {
       question: "¿Cómo se llama la cordillera más alta del mundo?",
-      answer: "Himalaya",
+      answers: ["Himalaya", "el Himalaya", "la cordillera del Himalaya"],
       hints: ["Está en Asia", "Tiene el Everest"],
       difficulty: 2,
       photo: "himalaya.png"
     },
     {
       question: "¿Qué océano está entre África y Australia?",
-      answers: ["Océano Índico", "Índico"],
+      answers: ["Océano Índico", "Índico", "el Océano Índico", "el Índico"],
       hints: ["Está cerca de la India", "No es el Atlántico"],
       difficulty: 2,
       photo: "indico.png"
     },
     {
       question: "¿Qué tipo de paisaje es el desierto del Sahara?",
-      answer: "desierto",
+      answers: ["desierto", "el desierto", "un desierto"],
       hints: ["Es muy caluroso", "Tiene mucha arena", "Te he dado una pista en la pregunta"],
       difficulty: 1,
       photo: "desierto.png"
@@ -165,55 +142,77 @@ module.exports = {
   ANIMALES: [
     {
       question: "¿Qué animal tiene trompa y las orejas grandes?",
-      answer: "elefante",
+      answers: ["elefante", "el elefante"],
       hints: ["Es gris", "Vive en África o en Asia"],
       difficulty: 1,
       photo: "elefante.png"
     },
     {
       question: "¿Qué animal pone huevos y nada en el agua?",
-      answer: "pato",
+      answers: ["pato", "el pato", "los patos", "patos", "patitos", "los patitos"],
       hints: ["Hace 'cuac'", "Tiene pico"],
       difficulty: 1,
       photo: "pato.png"
     },
     {
       question: "¿Qué animal doméstico ronronea?",
-      answer: "gato",
+      answers: ["gato", "el gato", "los gatos", "gatos"],
       hints: ["Tiene bigotes", "Es muy independiente"],
       difficulty: 1,
       photo: "gato.png"
-    }
+    },
+    {
+      question: "¿Qué animal se conoce como el 'rey de la selva'?",
+      answers: ["león", "el león", "los leones"],
+      hints: ["Tiene melena", "Es un felino grande"],
+      difficulty: 1,
+      photo: "leon.png"
+    },
+  
+    {
+      question: "¿Cómo se llama el animal que vive en el Polo Sur?",
+      answers: ["oso polar", "el oso polar", "los osos polares", "osos polares"],
+      hints: ["Es blanco","Come focas", "Es muy grande y tiene pelo"],
+      difficulty: 1,
+      photo: "oso_polar.png"
+    },
   ],
 
   COMIDA: [
     {
       question: "¿Qué fruta se asocia con Newton y la gravedad?",
-      answer: "manzana",
+      answers: ["manzana", "la manzana", "las manzanas", "manzanas"],
       hints: ["Es roja o verde", "Cae de los árboles"],
       difficulty: 1,
       photo: "manzana.png"
     },
     {
       question: "¿Qué plato famoso español se prepara con arroz?",
-      answer: "paella",
+      answers: ["paella", "la paella"],
       hints: ["Es típico de Valencia", "Se cocina en una sartén grande"],
       difficulty: 1,
       photo: "paella.png"
+    },
+    {
+      question: "¿Qué fruta tropical es amarilla por fuera y blanca por dentro?",
+      answers: ["el plátano", "platano", "los platanos", "platanos"],
+      hints: ["Se pela", "Es rica en potasio", "A los monos les gusta mucho"],
+      difficulty: 1,
+      photo: "platano.png"
     }
   ],
 
   OBJETOANTIGUO: [
     {
       question: "¿Cómo se llama el aparato que se usaba para escuchar música con cintas?",
-      answers: ["walkman","gualcman","gualman"],
+      answers: ["walkman","gualcman","gualman", "el walkman"],
       hints: ["Era de Sony", "Usaba casetes"],
       difficulty: 2,
       photo: "walkman.png"
     },
     {
       question: "¿Qué objeto servía para escribir antes de los ordenadores?",
-      answer: "máquina de escribir",
+      answers: ["máquina de escribir", "la máquina de escribir", "máquinas de escribir", "las máquinas de escribir"],
       hints: ["Tenía teclas", "Hacía ruido al escribir"],
       difficulty: 1,
       photo: "maquina_escribir.png"
@@ -223,63 +222,63 @@ module.exports = {
   TRABAJO: [
     {
       question: "¿Qué profesional trabaja con ladrillos y cemento?",
-      answer: "albañil",
+      answers: ["albañil", "el albañil"],
       hints: ["Construye casas", "Usa paleta", "Empieza por la letra A"],
       difficulty: 1,
       photo: "alban%CC%83il.png"
     },
     {
       question: "¿Quién atiende a los clientes en un restaurante?",
-      answer: "camarero",
+      answers: ["camarero", "el camarero"],
       hints: ["Sirve comida", "Trabaja con delantal", "Empieza por la letra C"],
       difficulty: 1,
       photo: "camarero.png"
     },
     {
       question: "¿Quién segaba el trigo con hoz en verano?",
-      answer: "segador",
+      answers: ["segador", "el segador"],
       hints: ["Trabaja en la siega", "Usa una hoz", "Empieza por la letra S"],
       difficulty: 2,
       photo: "segador.png"
     },
     {
       question: "¿Quién lleva mulas o burros cargados por el campo?",
-      answer: "arriero",
+      answers: ["arriero", "el arriero"],
       hints: ["Es alguien que guía animales", "Esta persona transporta carga", "Empieza por la letra A"],
       difficulty: 2,
       photo: "arriero.png"
     },
     {
       question: "¿Cómo se llama la mujer que ayudaba en los partos en el pueblo?",
-      answer: "comadrona",
+      answers: ["comadrona", "la comadrona"],
       hints: ["Sin el médico", "Ayudaba a dar a luz", "Empieza por la letra C"],
       difficulty: 2,
       photo: "comadrona.png"
     },
     {
       question: "¿Quién arreglaba los cacharros de barro o hacía botijos?",
-      answer: "alfarero",
+      answers: ["alfarero", "el alfarero"],
       hints: ["Es alguien que trabaja con barro", "Esta persona hace vasijas", "Empieza por la letra A"],
       difficulty: 2,
       photo: "alfarero.png"
     },
     {
       question: "¿Qué oficio tenía el que vendía pescado en los pueblos?",
-      answer: "pescadero",
+      answers: ["pescadero", "el pescadero"],
       hints: ["Es una persona que vende por ejemplo sardinas", "Esta persona a veces iba en furgoneta", "Empieza por la letra P"],
       difficulty: 2,
       photo: "pescadero.png"
     },
     {
       question: "¿Quién hacía esparto o pleita para sillas y capazos?",
-      answer: "espartero",
+      answers: ["espartero", "el espartero"],
       hints: ["Es una persona que trabaja con esparto", "Esta persona hace sillas y esteras", "Empieza por la letra E"],
       difficulty: 2,
       photo: "espartero.png"
     },
     {
       question: "¿Cómo se llama el que trabaja en la viña recogiendo uvas?",
-      answer: "vendimiador",
+      answers: ["vendimiador", "el vendimiador"],
       hints: ["Es una persona que trabaja en la vendimia", "Esta persona recoge uvas", "Empieza por la letra V"],
       difficulty: 2,
       photo: "vendimiador.png"
@@ -289,21 +288,21 @@ module.exports = {
   TECNOLOGÍA: [
     {
       question: "¿Cómo se llama el aparato con el que hacemos llamadas?",
-      answers: ["teléfono", "movil", "telefono movil"],
+      answers: ["teléfono", "movil", "telefono movil", "el telefono", "el movil", "el telefono movil"],
       hints: ["La mayoria tienen una pantalla táctil, pero algunos pueden tener teclado", "Sirve para comunicarnos", "Los adolescentes lo usan mucho"],
       difficulty: 1,
       photo: "movil.png"
     },
     {
       question: "¿Qué aparato sirve para ver películas y noticias?",
-      answers: ["televisor", "tele", "television"],
+      answers: ["televisor", "tele", "television", "la tele", "el televisor", "la televisión"],
       hints: ["Tiene una pantalla grande", "Lo solemos usar en el salón", "Empieza por la letra T"],
       difficulty: 1,
       photo: "tv.png"
     },
     {
       question: "¿Como se llama el reproductor de CD portátil creado por Sony?",
-      answer: "discman",
+      answers: ["discman", "el discman"],
       hints: ["Permite escuchar música en CD", "Empieza por la letra D"],
       difficulty: 1,
       photo: "discman.png"
@@ -313,21 +312,21 @@ module.exports = {
   ACTIVIDAD_FÍSICA: [
     {
       question: "¿Cómo se llama el ejercicio que consiste en caminar a paso rápido?",
-      answer: "marcha",
+      answers: ["marcha","la marcha"],
       hints: ["No se corre, se anda rápido", "Es bueno para las piernas", "Empieza por la letra M"],
       difficulty: 1,
       photo: "marcha.png"
     },
     {
       question: "¿Cómo se llama el juego en el que participaban chicas y saltaban mientras cantaban canciones?",
-      answers: ["salto a la comba", "salto de la comba", "la comba"],
+      answers: ["salto a la comba", "salto de la comba", "la comba", "el salto a la comba"],
       hints: ["Se usaba una comba", "Jugaban principalmente chicas", "Empieza por la letra S"],
       difficulty: 1,
       photo: "comba.png"
     },
     {
       question: "¿Cómo se llama el juego en el que participaban chicos y se saltaban unos a otros?",
-      answers: ["burro", "piola"],
+      answers: ["burro", "piola", "el burro", "la piola"],
       hints: ["Tiene nombre de animal", "Jugaban principalmente chicos", "Empieza por la letra B"],
       difficulty: 1,
       photo: "burro.png"
@@ -336,7 +335,7 @@ module.exports = {
   DE_EPOCA: [
     {
       question: "¿Qué canción de los 60 empezaba con 'Para bailar la Bamba se necesita una poca de gracia'?",
-      answer: "Bamba",
+      answers: ["La Bamba ", "Bamba", "la cancion de la Bamba"],
       hints: ["Fue popularizada por Ritchie Valens", "Es de origen mexicano", "Empieza por la letra B" ],
       difficulty: 2,
       photo: "bamba.png"
@@ -371,14 +370,14 @@ module.exports = {
     },
     {
       question: "¿Qué famoso coche de los 70 se conocía como 'el seiscientos'?",
-      answers: ["SEAT 600","seat", "SEAT", "seat seiscientos", "seat seisciento"],
+      answers: ["SEAT 600","seat", "SEAT", "seat seiscientos", "seat seisciento", "el seat seiscientos", "el seat 600"],
       hints: ["Fue el primer coche de muchas familias", "Era muy pequeño", "Empieza por la letra S"],
       difficulty: 1,
       photo: "seat.png"
     },
     {
       question: "¿Qué refresco se anunciaba con 'La chispa de la vida' en los 80?",
-      answer: "Coca-Cola",
+      answers: ["Coca-Cola", "la coca-cola"],
       hints: ["Es de color oscuro y tiene gas", "Tiene cafeína", "Empieza por la letra C"],
       difficulty: 1,
       photo: "cocacola.png"
@@ -393,7 +392,7 @@ module.exports = {
   ],
   FINAL: [
     {
-      question: "¿Qué famoso cantante español interpretó 'La Llamada' en los años 70?",
+      question: "¿Qué famoso cantante español interpretó 'Me olvidé de vivir' en los años 70?",
       answers: ["Julio Iglesias", "Julio"],
       hints: ["Es un cantante internacional", "También conocido por canciones románticas", "Empieza por la letra J"],
       difficulty: 2,
@@ -401,24 +400,17 @@ module.exports = {
     },
     {
       question: "¿Qué juguete consistía en piezas de plástico de colores que se encajaban entre sí?",
-      answer: "Lego",
+      answers: ["Lego", "el lego", "los lego"],
       hints: ["Muy popular entre niños y adolescentes", "Empieza por la letra L", "Vienen en cajas de colores"],
       difficulty: 1,
       photo: "lego.png"
     },
     {
       question: "¿Cómo se llamaba la famosa muñeca que venía con ropa y accesorios intercambiables?",
-      answer: "Barbie",
+      answers: ["Barbie", "la Barbie"],
       hints: ["Su nombre es el de la hija de la creadora", "Se lanzó en 1959", "Tiene muchos vestidos y accesorios", "Su nombre empieza con B"],
       difficulty: 1,
       photo: "barbie.png"
-    },
-    {
-      question: "¿Qué electrodoméstico se utilizaba para planchar la ropa?",
-      answer: "Plancha",
-      hints: ["Se enchufa a la corriente", "Es metálica y se calienta", "Se usa para que la ropa no tenga arrugas", "Su nombre empieza con P" ],
-      difficulty: 1,
-      photo: "plancha.png"
     },
     {
       question: "¿Qué famoso director de cine español rodó películas como 'El verdugo'?",
@@ -429,14 +421,14 @@ module.exports = {
     },
     {
       question: "¿Qué baile popular se hace en parejas, con movimientos de giro y abrazo?",
-      answer: "Vals",
-      hints: ["Música clásica o de salón", "La música tiene un ritmo de tres tiempos", "Empieza por la letra V"],
+      answers: ["Vals", "el vals"],
+      hints: ["Se baila con música clásica o de salón", "La música tiene un ritmo de tres tiempos", "Empieza por la letra V"],
       difficulty: 1,
       photo: "vals.png"
     },
     {
       question: "¿Qué bebida alcohólica española se obtiene de la uva y se suele servir en una copa?",
-      answer: "Vino",
+      answers: ["Vino", "el vino"],
       hints: ["Empieza por la letra V", "Puede ser tinto, blanco o rosado", "Es una bebida que se toma en comidas y celebraciones"],
       difficulty: 1,
       photo: "vino.png"
@@ -450,14 +442,14 @@ module.exports = {
     },
     {
       question: "¿Qué dulce típico español se hace de almendra y azúcar y se come mucho en Navidad?",
-      answer: "Turrón",
+      answers: ["Turrón", "el turrón"],
       hints: ["Hay blando y duro", "Es de Alicante y Jijona", "Su nombre empieza con T"],
       difficulty: 1,
       photo: "turron.png"
     },
     {
       question: "¿Qué medio de transporte urbano funciona con vías de metal y electricidad por encima?",
-      answer: "Tranvía",
+      answers: ["Tranvía", "el tranvía"],
       hints: ["Pasaba por la ciudad", "Se movía sobre raíles", "Antes de los metros, se usaba mucho en ciudades", "Empieza con T"],
       difficulty: 2,
       photo: "tranvia.png"
